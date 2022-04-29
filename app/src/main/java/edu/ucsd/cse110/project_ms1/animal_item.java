2bb2d43014df41c5dc3fba5b9725826368624249
Wangdong Xu <w6xu@ucsd.edu>
2022-04-28 18:15:28 -0700

    animal class

---
+++ app/src/main/java/edu/ucsd/cse110/project_ms1/animal_item.java
@@ -0,0 +1,15 @@
+package edu.ucsd.cse110.project_ms1;
+
+public class animal_item {
+    public String name; //essentially, the name is the tag in json file
+    public ZooData.VertexInfo exhibit;
+    public boolean selected;
+    public int order; // used for displaying on the screen
+
+    //not sure if i will change this constructor
+    public animal_item(String name, int order, ZooData.VertexInfo exhibit){
+        this.name = name;
+        this.order = order;
+        this.exhibit = exhibit;
+    }
+}
