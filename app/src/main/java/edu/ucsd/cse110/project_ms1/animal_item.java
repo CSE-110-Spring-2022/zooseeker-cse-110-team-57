f377861bdcd7d1980521bd7a76bdb7b93c6a4158
Wangdong Xu <w6xu@ucsd.edu>
2022-04-28 20:36:23 -0700

    animal class load json

--- app/src/main/java/edu/ucsd/cse110/project_ms1/animal_item.java
+++ app/src/main/java/edu/ucsd/cse110/project_ms1/animal_item.java
@@ -1,5 +1,20 @@
 package edu.ucsd.cse110.project_ms1;
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 public class animal_item {
     public String name; //essentially, the name is the tag in json file
     public ZooData.VertexInfo exhibit;
@@ -13,4 +28,18 @@ public class animal_item {
         this.order = order;
         this.exhibit = exhibit;
     }
+
+    public static List<animal_item> loadJSON(Context context, String path){
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON("sample_node_info.json");
+
+        List<animal_item> retVal =  new ArrayList<animal_item>();
+        int i =0;
+        for (Map.Entry<String, ZooData.VertexInfo> set : vInfo.entrySet()) {
+
+            retVal.add(new animal_item(set.getValue().name,i,set.getValue()));
+            i++;
+        }
+        return  retVal;
+    }
+
 }
