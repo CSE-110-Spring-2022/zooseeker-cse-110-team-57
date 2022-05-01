b8b2492395deb2c838322250bfc47576983d56ea
Wangdong Xu <w6xu@ucsd.edu>
2022-05-01 15:50:48 -0700

    merged to push

---
+++ app/src/main/java/edu/ucsd/cse110/project_ms1/AnimalItemDatabase.java
@@ -0,0 +1,34 @@
+package edu.ucsd.cse110.project_ms1;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+
+@Database(entities = {AnimalItem.class}, version = 1)
+public abstract  class AnimalItemDatabase extends RoomDatabase {
+    public abstract AnimalItemDao AnimalItemDao();
+
+    private  static  AnimalItemDatabase makeDatabase(Context context){
+        return Room.databaseBuilder(context, AnimalItemDatabase.class, "zoo.db").allowMainThreadQueries()
+                .addCallback(new Callback() {
+                    @Override
+                    public  void onCreate(@NonNull SupportSQLiteDatabase db){
+                        super.onCreate(db);
+                        Executors.newSingleThreadScheduledExecutor().execute(()->{
+                            List<AnimalItem> todos = AnimalItem
+                                    .search_by_tag(null);
+                            //getSingleton(context).todoListItemDao().insertAll(todos);
+                        });
+                    }
+                }).build();
+    }
+}
+
+
---
+++ app/src/test/java/edu/ucsd/cse110/project_ms1/AnimalItemDataBaseTest.java
@@ -0,0 +1,9 @@
+package edu.ucsd.cse110.project_ms1;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class AnimalItemDataBaseTest {
+}
