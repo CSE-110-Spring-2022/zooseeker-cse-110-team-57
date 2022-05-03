9620f54f2a9d1db238e540a341994ccaab4d6a92
Avery-l-code <85840653+Avery-l-code@users.noreply.github.com>
2022-05-03 14:59:48 -0700

    Create AddToListActivityTest.java

---
+++ app/src/androidTest/java/edu/ucsd/cse110/project_ms1/AddToListActivityTest.java
@@ -0,0 +1,16 @@
+package edu.ucsd.cse110.project_ms1;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+import android.widget.SearchView;
+
+@RunWith(AndroidJUnit4.class)
+public class AddToListActivityTest {
+    AnimalItemDatabase testDb;
+    AnimalItemDao animalItemDao;
+}
