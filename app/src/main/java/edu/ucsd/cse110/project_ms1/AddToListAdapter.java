e56e1eeab502d52ae79b538b4a25f97b3fb443d6
Avery-l-code <85840653+Avery-l-code@users.noreply.github.com>
2022-05-03 21:24:58 -0700

    64dp

--- app/src/main/java/edu/ucsd/cse110/project_ms1/AddToListAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/project_ms1/AddToListAdapter.java
@@ -38,6 +38,7 @@ public class AddToListAdapter extends RecyclerView.Adapter<AddToListAdapter.View
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.setAnimalItem(animalItems.get(position));
+
     }
 
     @Override
