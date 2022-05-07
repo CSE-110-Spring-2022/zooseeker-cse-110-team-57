c42c2c12b3d8db8aef28983296b18c7a6c95538b
Avery-l-code <85840653+Avery-l-code@users.noreply.github.com>
2022-05-06 22:58:30 -0700

    US9&10 with bug

--- app/src/main/java/edu/ucsd/cse110/project_ms1/DirectionActivity.java
+++ app/src/main/java/edu/ucsd/cse110/project_ms1/DirectionActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,38 +25,51 @@ import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 public class DirectionActivity extends AppCompatActivity {
     public DirectionAdapter direction_adapter;
     public RecyclerView direction_recyclerView;
+    public TextView goalExhibitTitle, startExhibitTitle;
     public StringAndAnimalItem stringAndAnimalItem;
-    private ArrayList<String> orderedAnimalNameString;
+    private ArrayList<String> orderedAnimalNameString, edgeStringList, edgeWeightList;
     private List<AnimalItem> orderedAnimalItemList;
-    private ArrayList<String> edgeStringList;
-    private ArrayList<String> edgeWeightList;
     private List<String> directionStringList;
     public GraphPath<String, IdentifiedWeightedEdge> path;
-    public String start;
-    public String goal;
+    public String start_id, start_name, goal_id, goal_name;
+    public String entrance_id, entrance_name;
+    public int directionItemOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direction);
-
+        //initiate field
         edgeStringList = new ArrayList<String>();
         edgeWeightList = new ArrayList<String>();
         directionStringList = new ArrayList<String>();
         orderedAnimalItemList = new ArrayList<AnimalItem>();
         stringAndAnimalItem = new StringAndAnimalItem();
+        directionItemOrder = 0;
+        entrance_id = "entrance_exit_gate";
+        entrance_name = "Entrance and Exit Gate";
 
+        //adapter
         direction_adapter = new DirectionAdapter();
         direction_adapter.setHasStableIds(true);
         direction_recyclerView = this.findViewById(R.id.all_direction_items);
         direction_recyclerView.setLayoutManager(new LinearLayoutManager(this));
         direction_recyclerView.setAdapter(direction_adapter);
+        startExhibitTitle = findViewById(R.id.start_exhibit_name);
+        goalExhibitTitle = findViewById(R.id.goal_exhibit_name);
 
         //get the ordered Animal name string
         Intent intent = getIntent();
         orderedAnimalNameString = intent.getStringArrayListExtra("routedAnimalNameList");
+        orderedAnimalItemList = loadOrderedAnimals(orderedAnimalNameString);
 
+        //set the first Exhibit
+        displayDirection(directionItemOrder);
+    }
+
+    public List<AnimalItem> loadOrderedAnimals(List<String> orderedAnimalNameString){
+        List<AnimalItem> orderedAnimalItemList = new ArrayList<AnimalItem>();
         SharedPreferences sharedPreferences = getSharedPreferences("Team57", 0);
         for (String animalName: orderedAnimalNameString){
             //find the string containing all related information of a selected animal
@@ -65,31 +80,65 @@ public class DirectionActivity extends AppCompatActivity {
             //add animalItem to the AnimalItem list
             orderedAnimalItemList.add(animalItem);
         }
+        return orderedAnimalItemList;
+    }
 
-
-        //Get the first Animal Exhibit Name
-        start = "entrance_exit_gate";
-        goal = orderedAnimalItemList.get(0).id;
-
-        path = DijkstraShortestPath.findPathBetween(AnimalItem.gInfo, start, goal);
+    public void setDirectionList(String startId, String goalId, String startName, String goalName){
+        path = DijkstraShortestPath.findPathBetween(AnimalItem.gInfo, startId, goalId);
         for (IdentifiedWeightedEdge edge: path.getEdgeList()){
             String currentEdge = AnimalItem.eInfo.get(edge.getId()).street;
             edgeStringList.add(currentEdge);
             edgeWeightList.add(Double.toString(AnimalItem.gInfo.getEdgeWeight(edge)));
         }
-
         for (int i = 0; i < edgeStringList.size(); i++){
-            String displayInfo = "Walk "+edgeWeightList.get(i)+" along "+edgeStringList.get(i)+" from "+start+" to "+goal+".";
+            String displayInfo = "Walk "+edgeWeightList.get(i)+"ft along "+edgeStringList.get(i)
+                    +" from "+startName+" to "+goalName+".";
             directionStringList.add(displayInfo);
         }
+    }
 
+    public void displayDirection(int order){
+        //set the start and goal
+        if (order != 0){
+            start_id = goal_id;
+            start_name = goal_name;
+            goal_id = orderedAnimalItemList.get(order).id;
+            goal_name = orderedAnimalItemList.get(order).name;
+        }
+        else{
+            start_id = entrance_id;
+            start_name = entrance_name;
+            goal_id = orderedAnimalItemList.get(0).id;
+            goal_name = orderedAnimalItemList.get(0).name;
+        }
+        //display directions
+        String startExhibit = "From: "+start_name;
+        String goalExhibit = "To: "+goal_name;
+        startExhibitTitle.setText(startExhibit);
+        goalExhibitTitle.setText(goalExhibit);
+        setDirectionList(start_id, goal_id, start_name, goal_name);
         direction_adapter.setDirectionsStringList(directionStringList);
-        
     }
-    /*
-    public void saveOrderedAnimals(){
-        SharedPreferences sharedPreferences = getSharedPreferences("Team57", 0);
-    }
-    */
 
+    public void onNextButtonClick(View view) {
+        if (directionItemOrder == orderedAnimalItemList.size() - 1){
+            Utilities.showAlert(this, "This is the end of your tour");
+            return;
+        }
+        else{
+            directionItemOrder++;
+            displayDirection(directionItemOrder);
+        }
+    }
+
+    public void onBackButtonClick(View view) {
+        if (directionItemOrder == 0){
+            Utilities.showAlert(this, "This is the beginning of your tour");
+            return;
+        }
+        else{
+            directionItemOrder--;
+            displayDirection(directionItemOrder);
+        }
+    }
 }
