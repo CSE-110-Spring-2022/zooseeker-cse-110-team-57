2d9b2927a5bcffe6cc7cded74b062bc5959e206e
Avery-l-code <85840653+Avery-l-code@users.noreply.github.com>
2022-05-08 16:09:30 -0700

    US5_duplicated_test

---
+++ app/src/androidTest/java/edu/ucsd/cse110/project_ms1/US5_duplicated_test.java
@@ -0,0 +1,135 @@
+package edu.ucsd.cse110.project_ms1;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class US5_duplicated_test {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void uS5_duplicated_test() {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.selected_animals_number), withText("0"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("0")));
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.search_bar_2), withContentDescription("Search"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(androidx.appcompat.R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction searchAutoComplete = onView(
+                allOf(withId(androidx.appcompat.R.id.search_src_text),
+                        childAtPosition(
+                                allOf(withId(androidx.appcompat.R.id.search_plate),
+                                        childAtPosition(
+                                                withId(androidx.appcompat.R.id.search_edit_frame),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.add_to_button), withText("Add"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.all_searched_animals),
+                                        1),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.selected_animals_number), withText("1"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView2.check(matches(withText("1")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.an_selected_animal), withText("Lions"),
+                        withParent(withParent(withId(R.id.all_selected_animals))),
+                        isDisplayed()));
+        textView3.check(matches(withText("Lions")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.an_selected_animal), withText("Lions"),
+                        withParent(withParent(withId(R.id.all_selected_animals))),
+                        isDisplayed()));
+        textView4.check(matches(withText("Lions")));
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.add_to_button), withText("Add"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.all_searched_animals),
+                                        1),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction textView5 = onView(
+                allOf(withId(R.id.selected_animals_number), withText("1"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView5.check(matches(withText("1")));
+
+        ViewInteraction textView6 = onView(
+                allOf(withId(R.id.an_selected_animal), withText("Lions"),
+                        withParent(withParent(withId(R.id.all_selected_animals))),
+                        isDisplayed()));
+        textView6.check(matches(withText("Lions")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
