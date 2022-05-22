b0bc121fb64c202436b58f52d2af8e20db51f806
zibingao <zigao@ucsd.edu>
2022-05-22 16:09:15 -0700

    Create ChoiceActivity.java

---
+++ app/src/main/java/edu/ucsd/cse110/project_ms1/ChoiceActivity.java
@@ -0,0 +1,40 @@
+package edu.ucsd.cse110.project_ms1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.SearchView;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+
+public class ChoiceActivity extends AppCompatActivity{
+
+}
