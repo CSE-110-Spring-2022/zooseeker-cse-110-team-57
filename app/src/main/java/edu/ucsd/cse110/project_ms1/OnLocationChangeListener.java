18dedf8f01f7e1dbe5fb1bc9da7464531a57d4c7
Avery-l-code <85840653+Avery-l-code@users.noreply.github.com>
2022-05-22 16:29:03 -0700

    US0 locationchangelistsner

---
+++ app/src/main/java/edu/ucsd/cse110/project_ms1/OnLocationChangeListener.java
@@ -0,0 +1,7 @@
+package edu.ucsd.cse110.project_ms1;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public interface OnLocationChangeListener {
+    void OnLocationChange(LatLng current);
+}
