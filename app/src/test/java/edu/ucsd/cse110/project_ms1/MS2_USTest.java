5a994cb4fab033153c8ad0217ea91e8454ff49c5
Avery-l-code <85840653+Avery-l-code@users.noreply.github.com>
2022-05-22 21:29:59 -0700

    US0 Test

---
+++ app/src/test/java/edu/ucsd/cse110/project_ms1/MS2_USTest.java
@@ -0,0 +1,29 @@
+package edu.ucsd.cse110.project_ms1;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+@RunWith(AndroidJUnit4.class)
+public class MS2_USTest {
+    @Test
+    public void Mocking_of_Location(){
+
+    }
+}
