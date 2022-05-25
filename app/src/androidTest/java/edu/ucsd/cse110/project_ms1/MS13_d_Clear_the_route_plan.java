e3324f063229baf2f95e5e947a6997984769e394
weihuapan00 <w5pan@ucsd.edu>
2022-05-24 19:54:58 -0700

    test

--- app/src/androidTest/java/edu/ucsd/cse110/project_ms1/MS13_d_Clear_the_route_plan.java
+++ app/src/androidTest/java/edu/ucsd/cse110/project_ms1/MS13_d_Clear_the_route_plan.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class ClearBtn {
+public class MS13_d_Clear_the_route_plan {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
