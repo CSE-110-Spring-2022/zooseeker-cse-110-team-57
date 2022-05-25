f66016da3f0053a3319f1160e43de0b9e657acbb
Avery-l-code <85840653+Avery-l-code@users.noreply.github.com>
2022-05-24 19:55:42 -0700

    US0 group_id

--- app/src/main/java/edu/ucsd/cse110/project_ms1/ZooData.java
+++ app/src/main/java/edu/ucsd/cse110/project_ms1/ZooData.java
@@ -28,20 +28,20 @@ public class ZooData {
             @SerializedName("exhibit_group") EXHIBIT_GROUP
         }
 
-        VertexInfo(String id, Kind kind, String name, List<String> tags, Double lat, Double lng, String parent_id) {
+        VertexInfo(String id, Kind kind, String name, List<String> tags, Double lat, Double lng, String group_id) {
             this.id = id;
             this.kind = kind;
             this.name = name;
             this.tags = tags;
             this.lat = lat;
             this.lng = lng;
-            this.parent_id = parent_id;
+            this.group_id = group_id;
         }
 
         public String id;
         public Kind kind;
         public String name;
-        public String parent_id;
+        public String group_id;
         public List<String> tags;
         public Double lat, lng;
     }
