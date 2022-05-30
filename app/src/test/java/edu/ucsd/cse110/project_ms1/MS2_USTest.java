82764b08f7559cb5c0aedf2b8038cc35e984ea7e
weihuapan00 <w5pan@ucsd.edu>
2022-05-30 01:04:33 -0700

    update test

--- app/src/test/java/edu/ucsd/cse110/project_ms1/MS2_USTest.java
+++ app/src/test/java/edu/ucsd/cse110/project_ms1/MS2_USTest.java
@@ -305,7 +305,7 @@ public class MS2_USTest {
                 new LatLng(32.73561,-117.14936));
 
         AnimalItem.loadInfo(context, vPath, ePath, gPath);
-        animalItemArrayList.add(a4);
+
         animalItemArrayList.add(a1);
         animalItemArrayList.add(a2);
         animalItemArrayList.add(a3);
@@ -314,15 +314,62 @@ public class MS2_USTest {
 
         List<route_node> route = AnimalItem.plan_route(animalItemArrayList,a4.id);
 
-        List<route_node> r = AnimalUtilities.reroute(2,  route, latlng, going_forward);
+        for (route_node node : route){
+            System.out.println(node.names);
+        }
+
+        List<route_node> r = AnimalUtilities.reroute(0,  route, latlng, going_forward);
         for (route_node node : r){
             System.out.println(node.names);
         }
+        //wait for reroute
         //??
     }
 
 
+    @Test
+    public void findStartPoint() throws IOException {
 
+    }
+
+    @Test
+    public void checkOffRoute() throws IOException{
+        //load zooData
+        String vPath = "exhibit_info.json";
+        String ePath = "trail_info.json";
+        String gPath = "zoo_graph.json";
+        Context context = ApplicationProvider.getApplicationContext();
+
+        LatLng latlng = new LatLng(32,-117);
+        boolean going_forward =  true;
+        ArrayList<AnimalItem> animalItemArrayList = new ArrayList<>();
+        AnimalItem a1 = new AnimalItem("orangutan",
+                new ArrayList<>(),
+                "Orangutans",
+                new LatLng(32.736864688333235,-117.16364410510093));
+        AnimalItem a2 = new AnimalItem("siamang",
+                new ArrayList<>(),
+                "Siamangs",
+                new LatLng(32.736864688333235,-117.16079397323202));
+        AnimalItem a3 = new AnimalItem("toucan",
+                new ArrayList<>(),
+                "Toucan",
+                new LatLng(32.736864688333235,-117.16079397323202));
+        AnimalItem a4 = new AnimalItem("entrance_exit_gate",
+                new ArrayList<>(),
+                "Entrance and Exit Gate",
+                new LatLng(32.73561,-117.14936));
+
+        AnimalItem.loadInfo(context, vPath, ePath, gPath);
+
+        animalItemArrayList.add(a1);
+        animalItemArrayList.add(a2);
+        animalItemArrayList.add(a3);
+        animalItemArrayList.add(a4);
+
+
+        List<route_node> route = AnimalItem.plan_route(animalItemArrayList,a4.id);
+    }
 
 
 }
